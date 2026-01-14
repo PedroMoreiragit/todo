a673471c9bbdd17cea33482dedcd1c2cb183079b
Pedromoreiragit <pedromoreirat242@gmail.com>
2026-01-14 16:10:02 -0300

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import Text from "./components/text";
+
+export default function App() {
+
+
+  return (
+    <>
+      <div className="flex flex-col gap-2">
+        <Text variant={"body-sm-bold"} className="text-pink-base">Hello World!</Text>
+        <Text className="text-green-base">Hello World!</Text>
+        <Text variant={"body-md-bold"}>Hello World!</Text>
+        <Text variant={"body-md-bold"}>Levar o dog para passear</Text>
+      </div>
+    </>
+  )
+}
