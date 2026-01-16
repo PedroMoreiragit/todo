0da6833d3b207fbe1f0a018321d0433d7c013a00
Pedromoreiragit <pedromoreirat242@gmail.com>
2026-01-16 17:36:18 -0300

    feat: list tasks

--- src/core-components/task-item.tsx
+++ src/core-components/task-item.tsx
@@ -8,11 +8,16 @@ import XIcon from "../assets/icons/x.svg?react";
 import CheckIcon from "../assets/icons/check.svg?react";
 import React from "react";
 import InputText from "../components/input-text";
+import { TaskState, type Task } from "../models/task";
+import { cx } from "class-variance-authority";
 
+interface TaskItemProps {
+    task: Task;
+}
 
-export default function TaskItem() {
+export default function TaskItem({task}: TaskItemProps) {
 
-    const [isEditing, setIsEditing] = React.useState(false);
+    const [isEditing, setIsEditing] = React.useState(task?.state ===  TaskState.Creating);
 
     function handleEditTask() {
         setIsEditing(true);
@@ -27,8 +32,8 @@ export default function TaskItem() {
             {!isEditing ?
 
                 <>
-                    <InputCheckbox />
-                    <Text className="flex-1">🛒 Fazer compras da semana</Text>
+                    <InputCheckbox value={task?.concluded?.toString()} checked={task.concluded} />
+                    <Text className={cx("flex-1", {'line-through': task?.concluded,})}>{task?.title}</Text>
                     <div className="flex gap-1">
                         <ButtonIcon icon={TrashIcon} variant="tertiary" />
                         <ButtonIcon icon={PencilIcon} variant="tertiary" onClick={handleEditTask} />
--- src/core-components/tasks-list.tsx
+++ src/core-components/tasks-list.tsx
@@ -11,7 +11,6 @@ export default function TaskList() {
     const { tasks } = useTasks();
     const { prepareTask } = useTask();
 
-    console.log(tasks);
 
     function handleNewTask() {
         prepareTask()
@@ -24,10 +23,7 @@ export default function TaskList() {
             </section>
 
             <section className="space-y-2">
-                <TaskItem />
-                <TaskItem />
-                <TaskItem />
-                <TaskItem />
+                {tasks.map((task) => <TaskItem key={task.id} task={task} />)}
             </section>
         </>
     );
